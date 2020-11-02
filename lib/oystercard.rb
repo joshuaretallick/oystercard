4ec3e0962c94cdef5598e73a407763effbd827a2
Joshua Retallick <josh@old-empire.co.uk>
2020-11-02 16:53:08 +0000

    Enforced maximum balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,16 @@
 class Oystercard
-attr_reader :balance
+
+  MAXIMUM_BALANCE = 90
+
+  attr_reader :balance
 
   def initialize
     @balance = 0
   end
 
   def top_up(money)
+    raise "Maximum balance exceeded" if money + @balance > MAXIMUM_BALANCE
     @balance += money
   end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,4 +11,10 @@ describe Oystercard do
     expect{oystercard.top_up(10)}.to change{oystercard.balance}.by(10)
   end
 
+  it "should throw an error when top_up takes balance over £90" do
+    maximum_balance = Oystercard::MAXIMUM_BALANCE
+    oystercard.top_up(maximum_balance)
+    expect{ oystercard.top_up 1 }.to raise_error "Maximum balance exceeded"
+  end
+
 end
