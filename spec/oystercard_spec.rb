044e03681ef12f895b5564954b01e6ec3d43a042
Joshua Retallick <josh@old-empire.co.uk>
2020-11-02 15:59:04 +0000

    Refactored with change_by added to top_up test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,14 @@
 require "oystercard"
 
 describe Oystercard do
+  subject(:oystercard) {described_class.new}
 
   it "should return balance of 0 by default" do
-    expect(subject.balance).to eq 0
+    expect(oystercard.balance).to eq 0
   end
 
   it "should return a balance of 10 when top up 10" do
-    expect(subject.top_up(10)).to eq 10
+    expect{oystercard.top_up(10)}.to change{oystercard.balance}.by(10)
   end
 
 end
